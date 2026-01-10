b2772d34dd557f64d7dd95cf5da178b90e8a05b4
reshad21 <miakala7@gmail.com>
2026-01-10 11:23:09 +0600

    Add VideoGalleryHeader and loading components for video gallery feature

---
+++ src/app/(common)/video-gallery/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../_components/HeroSection'
+import VideoGalleryHeader from './_components/VideoGalleryHeader'
+
+const VideoGalleryPage = () => {
+  return (
+    <div>
+      <HeroSection title="Video Gallery" subtitle="Video Gallery" />
+      <VideoGalleryHeader/>
+    </div>
+  )
+}
+
+export default VideoGalleryPage
---
+++ src/app/(dashboard)/dashboard/video-gallery/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoGallerypage = () => {
+  return (
+    <div>
+      <h1>Video Gallery</h1>
+    </div>
+  )
+}
+
+export default VideoGallerypage
