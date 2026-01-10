18987ec95d7fc8b300daf47a265e9993f4364d5c
reshad21 <miakala7@gmail.com>
2026-01-10 18:10:54 +0600

    Refactor VideoGalleryCard and VideoGallerySection to remove unused props and enhance data handling; implement pagination in VideoGalleryPage

--- src/app/(common)/video-gallery/_components/VideoGallerySection.tsx
+++ src/app/(common)/video-gallery/_components/VideoGallerySection.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import VideoGalleryCard, { VideoGalleryCardProps } from "./VideoGalleryCard";
-import { getVideoGalleries } from "@/services/video-gallery";
 
-const VideoGallerySection = async () => {
-  const videoGalleryRes = await getVideoGalleries([]);
-  const videoGalleries = videoGalleryRes?.data?.data || [];
-  if (videoGalleries.length === 0) {
-    return (
-      <div className="text-center py-8 text-gray-500">No videos available.</div>
-    );
-  }
+
+const VideoGallerySection = async ({
+  videoGalleryData = [],
+}: {
+  videoGalleryData: VideoGalleryCardProps[];
+}) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto">
-      {videoGalleries?.map((videoGallery: VideoGalleryCardProps) => (
-        <VideoGalleryCard key={videoGallery.id} {...videoGallery}/>
-      ))}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto my-10">
+      {videoGalleryData.length === 0 ? (
+        <div className="text-center py-8 text-gray-500">
+          No videos available.
+        </div>
+      ) : (
+        videoGalleryData?.map((videoGallery: VideoGalleryCardProps) => (
+          <VideoGalleryCard key={videoGallery.id} {...videoGallery} />
+        ))
+      )}
     </div>
   );
 };
